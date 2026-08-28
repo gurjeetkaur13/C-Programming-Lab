8a9588aef0634cb392e6195cc3a73aa65c349d4a
Gurjeet Kaur <gurjeetgurjeet544@gmail.com>
2026-08-28 23:56:50 +0530

    Add Day 3 Question 2

---
+++ 100-Days-of-code/Day3_Q2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, temp;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swap: %d %d\n", a, b);
+
+    return 0;
+}
