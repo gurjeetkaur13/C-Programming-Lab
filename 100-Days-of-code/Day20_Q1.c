a30a231e037b681f4deeee693c0b8d637b443b6d
Gurjeet Kaur <gurjeetgurjeet544@gmail.com>
2026-08-29 17:39:23 +0530

    Add Day 20 Question 1

---
+++ 100-Days-of-code/Day20_Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, digit;
+    int product = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        digit = n % 10;
+
+        if (digit % 2 != 0)
+        {
+            product *= digit;
+        }
+
+        n /= 10;
+    }
+
+    printf("%d\n", product);
+
+    return 0;
+}
