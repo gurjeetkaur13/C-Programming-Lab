fb5817acecb43653d316fb0d14d82dff038ab6c1
Gurjeet Kaur <gurjeetgurjeet544@gmail.com>
2026-09-18 12:31:20 +0530

    Add Day 32 Question 2

---
+++ 100-Days-of-code/Day32_Q2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    long long n;
+    int freq[10] = {0};
+    int digit, i, max = 0, ans = 0;
+
+    scanf("%lld", &n);
+
+    while(n > 0)
+    {
+        digit = n % 10;
+        freq[digit]++;
+        n = n / 10;
+    }
+
+    for(i = 0; i < 10; i++)
+    {
+        if(freq[i] > max)
+        {
+            max = freq[i];
+            ans = i;
+        }
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
