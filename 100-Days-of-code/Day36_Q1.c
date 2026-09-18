88ee90c6bf9ce1a3158e198c5af95186f63843e8
Gurjeet Kaur <gurjeetgurjeet544@gmail.com>
2026-09-18 13:12:20 +0530

    Add Day 36 Question 1

---
+++ 100-Days-of-code/Day36_Q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    int a[10][10];
+    int i, j;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
