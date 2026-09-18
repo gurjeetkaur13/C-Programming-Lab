fa46804caf5eb794da50f3236408ce2cf74681fb
Gurjeet Kaur <gurjeetgurjeet544@gmail.com>
2026-09-18 13:49:41 +0530

    Add Day 37 Question 2

---
+++ 100-Days-of-code/Day37_Q2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    int a[10][10];
+    int i, j;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Print transpose
+    for(i = 0; i < c; i++)
+    {
+        for(j = 0; j < r; j++)
+        {
+            printf("%d ", a[j][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
