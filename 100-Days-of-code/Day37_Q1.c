abf7a62495ac70353f1e77ba2b6f66e4df9b6810
Gurjeet Kaur <gurjeetgurjeet544@gmail.com>
2026-09-18 13:43:54 +0530

    Add Day 37 Question 1

---
+++ 100-Days-of-code/Day37_Q1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int r, c;
+    int a[10][10];
+    int sum[10];
+    int i, j;
+
+    scanf("%d %d", &r, &c);
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        sum[i] = 0;
+
+        for(j = 0; j < c; j++)
+        {
+            sum[i] += a[i][j];
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        printf("%d ", sum[i]);
+    }
+
+    return 0;
+}
